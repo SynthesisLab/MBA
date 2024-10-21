04553ff75ad3a2267772c311e8f9376de2834ad3
Baptiste <baptiste.mouillon21@gmail.com>
2024-10-21 17:07:29 +0200

    New structure

---
+++ formula.h
@@ -0,0 +1,70 @@
+#ifndef FORMULA_H
+#define FORMULA_H
+
+#include <vector>
+#include <memory>
+#include <variant>
+
+using namespace std;
+
+class Formula;
+
+// Operators
+enum class UnOp { Not };
+enum class BinOp { Plus, Minus, Mult, And, Or, Xor };
+
+// Variables
+class Variable {
+public:
+    int var;
+
+    explicit Variable(int v);
+
+    uint64_t evaluate(const vector<uint64_t>& inputs) const;
+
+    string toString() const;
+};
+
+// Unary operations
+class UnaryOperation {
+public:
+    UnOp op;
+    shared_ptr<Formula> operand;
+
+    UnaryOperation(UnOp op, shared_ptr<Formula> operand);
+
+    uint64_t evaluate(const vector<uint64_t>& inputs) const;
+
+    string toString() const;
+};
+
+// Binary operations
+class BinaryOperation {
+public:
+    BinOp op;
+    shared_ptr<Formula> left;
+    shared_ptr<Formula> right;
+
+    BinaryOperation(BinOp op, shared_ptr<Formula> left, shared_ptr<Formula> right);
+
+    uint64_t evaluate(const vector<uint64_t>& inputs) const;
+
+    string toString() const;
+};
+
+// Formulas
+class Formula {
+public:
+    variant<Variable, BinaryOperation, UnaryOperation> expr;
+
+    Formula(Variable var);
+    Formula(BinaryOperation binOp);
+    Formula(UnaryOperation unOp);
+
+    uint64_t evaluate(const vector<uint64_t>& inputs) const;
+
+    string toString() const;
+    void printFormula() const;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,98 +1,21 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <set>
-#include <memory>
-#include <variant>
-#include <string>
+#include "formula.h"
 #include "json.hpp"
 
 using namespace std;
 using json = nlohmann::json;
 
-int numVar = 2;
-
-class Formula;
-
-// Unary operators
-enum class UnOp { Not };
-
-// Binary operators
-enum class BinOp { Plus, Minus, Mult, And, Or, Xor };
-
-// Variables
-class Variable
-{
-public:
-   int var;
-
-   explicit Variable(int v) : var(v) {}
-};
-
-// Unary operation
-class UnaryOperation
-{
-public:
-   UnOp op;
-   std::shared_ptr<Formula> operand;
-
-   UnaryOperation(UnOp op, std::shared_ptr<Formula> operand) : op(op), operand(operand) {}
-};
-
-// Binary operation
-class BinaryOperation
-{
-public:
-   BinOp op;
-   std::shared_ptr<Formula> left;
-   std::shared_ptr<Formula> right;
-
-   BinaryOperation(BinOp op, std::shared_ptr<Formula> left, std::shared_ptr<Formula> right) : op(op), left(left), right(right) {}
-};
-
-// Formula
-class Formula
-{
-public:
-   std::variant<Variable, BinaryOperation, UnaryOperation> expr;
-
-   Formula(Variable var) : expr(var) {}
-   Formula(BinaryOperation binOp) : expr(binOp) {}
-   Formula(UnaryOperation unOp) : expr(unOp) {}
-};
-
-// String of unary operator
-std::string to_string(UnOp op)
-{
-   switch (op)
-   {
-   case UnOp::Not: return "~";
-   default: throw std::runtime_error("Unknown unary operator");
-   }
-}
-
-// String of binary operator
-std::string to_string(BinOp op)
-{
-   switch (op)
-   {
-   case BinOp::Plus: return "+";
-   case BinOp::Minus: return "-";
-   case BinOp::Mult: return "*";
-   case BinOp::And: return "&";
-   case BinOp::Or: return "|";
-   case BinOp::Xor: return "^";
-   default: throw std::runtime_error("Unknown binary operator");
-   }
-}
+int numVar;
 
 // Generate formulas until size n
-std::vector<std::set<std::shared_ptr<Formula>>> generateFormulas(int n) {
-   std::vector<std::set<std::shared_ptr<Formula>>> formulas(n + 1);
+vector<set<shared_ptr<Formula>>> generateFormulas(int n) {
+   vector<set<shared_ptr<Formula>>> formulas(n + 1);
 
    // Formulas of size 1
    for (int i = 0; i < numVar; ++i) {
-      formulas[1].insert(std::make_shared<Formula>(Variable(i)));
+      formulas[1].insert(make_shared<Formula>(Variable(i)));
    }
 
    // Generation
@@ -100,19 +23,19 @@ std::vector<std::set<std::shared_ptr<Formula>>> generateFormulas(int n) {
 
       // Unary operators
       for (const auto& formula : formulas[size - 1]) {
-         formulas[size].insert(std::make_shared<Formula>(UnaryOperation(UnOp::Not, formula)));
+         formulas[size].insert(make_shared<Formula>(UnaryOperation(UnOp::Not, formula)));
       }
 
       // Binary operators
       for (int k = 1; k <= size - 2; ++k) {
          for (const auto& left : formulas[k]) {
             for (const auto& right : formulas[size - k - 1]) {
-               formulas[size].insert(std::make_shared<Formula>(BinaryOperation(BinOp::Plus, left, right)));
-               formulas[size].insert(std::make_shared<Formula>(BinaryOperation(BinOp::Minus, left, right)));
-               formulas[size].insert(std::make_shared<Formula>(BinaryOperation(BinOp::Mult, left, right)));
-               formulas[size].insert(std::make_shared<Formula>(BinaryOperation(BinOp::And, left, right)));
-               formulas[size].insert(std::make_shared<Formula>(BinaryOperation(BinOp::Or, left, right)));
-               formulas[size].insert(std::make_shared<Formula>(BinaryOperation(BinOp::Xor, left, right)));
+               formulas[size].insert(make_shared<Formula>(BinaryOperation(BinOp::Plus, left, right)));
+               formulas[size].insert(make_shared<Formula>(BinaryOperation(BinOp::Minus, left, right)));
+               formulas[size].insert(make_shared<Formula>(BinaryOperation(BinOp::Mult, left, right)));
+               formulas[size].insert(make_shared<Formula>(BinaryOperation(BinOp::And, left, right)));
+               formulas[size].insert(make_shared<Formula>(BinaryOperation(BinOp::Or, left, right)));
+               formulas[size].insert(make_shared<Formula>(BinaryOperation(BinOp::Xor, left, right)));
             }
          }
       }
@@ -121,37 +44,12 @@ std::vector<std::set<std::shared_ptr<Formula>>> generateFormulas(int n) {
    return formulas;
 }
 
-// Print formula
-void printFormula(const Formula& formula)
-{
-   std::visit([](auto&& arg) {
-      using T = std::decay_t<decltype(arg)>;
-
-      if constexpr (std::is_same_v<T, Variable>) {
-         std::cout << "Var(" << arg.var << ")";
-      }
-      else if constexpr (std::is_same_v<T, BinaryOperation>) {
-         std::cout << "(";
-         printFormula(*arg.left);
-         std::cout << " " << to_string(arg.op) << " ";
-         printFormula(*arg.right);
-         std::cout << ")";
-      }
-      else if constexpr (std::is_same_v<T, UnaryOperation>) {
-         std::cout << "(" << to_string(arg.op) << " ";
-         printFormula(*arg.operand);
-         std::cout << ")";
-      }
-      }, formula.expr);
-}
-
 // Print generated formulas
-void printFormulas(const std::vector<std::set<std::shared_ptr<Formula>>>& formulas) {
+void printFormulas(const vector<set<shared_ptr<Formula>>>& formulas) {
    for (size_t i = 1; i < formulas.size(); ++i) {
-      std::cout << "Formules de taille " << i << " :\n";
+      cout << "Formules de taille " << i << " :\n";
       for (const auto& formula : formulas[i]) {
-         printFormula(*formula);
-         std::cout << "\n";
+         formula->printFormula();
       }
    }
 }
@@ -160,18 +58,18 @@ void printFormulas(const std::vector<std::set<std::shared_ptr<Formula>>>& formul
 class Example
 {
 public:
-   std::vector<uint64_t> inputs; // Antecedents
+   vector<uint64_t> inputs; // Antecedents
    uint64_t output; // Image
 };
 
 // Read JSON file and extract examples
-std::vector<Example> readExamples(const std::string& filename) {
-   std::vector<Example> examples;
+vector<Example> readExamples(const string& filename) {
+   vector<Example> examples;
 
    // Read the JSON file
-   std::ifstream inputFile(filename);
+   ifstream inputFile(filename);
    if (!inputFile) {
-      throw std::runtime_error("Could not open the file!");
+      throw runtime_error("Could not open the file!");
    }
 
    json j;
@@ -182,13 +80,16 @@ std::vector<Example> readExamples(const std::string& filename) {
    const auto& initialInputs = j["initial"]["inputs"];
    const auto& initialOutput = j["initial"]["outputs"].begin().value(); // Assuming one output
 
+   // Set numVar based on the number of inputs
+   numVar = initialInputs.size();
+
    // Extract initial inputs
    for (const auto& input : initialInputs.items()) {
-      initialExample.inputs.push_back(std::stoull(input.value()["value"].get<std::string>(), nullptr, 16));
+      initialExample.inputs.push_back(stoull(input.value()["value"].get<string>(), nullptr, 16));
    }
 
    // Extract initial output
-   initialExample.output = std::stoull(initialOutput["value"].get<std::string>(), nullptr, 16);
+   initialExample.output = stoull(initialOutput["value"].get<string>(), nullptr, 16);
 
    // Add the initial example to the vector
    examples.push_back(initialExample);
@@ -201,11 +102,11 @@ std::vector<Example> readExamples(const std::string& filename) {
 
       // Extract inputs
       for (const auto& input : inputs.items()) {
-         example.inputs.push_back(std::stoull(input.value()["value"].get<std::string>(), nullptr, 16));
+         example.inputs.push_back(stoull(input.value()["value"].get<string>(), nullptr, 16));
       }
 
       // Extract output
-      example.output = std::stoull(output["value"].get<std::string>(), nullptr, 16);
+      example.output = stoull(output["value"].get<string>(), nullptr, 16);
 
       // Add the example to the vector
       examples.push_back(example);
@@ -214,49 +115,10 @@ std::vector<Example> readExamples(const std::string& filename) {
    return examples;
 }
 
-uint64_t evaluateUnaryOperation(UnOp op, uint64_t operandValue) {
-   switch (op) {
-   case UnOp::Not: return ~operandValue;
-   default: throw std::runtime_error("Unsupported unary operation");
-   }
-}
-
-uint64_t evaluateBinaryOperation(BinOp op, uint64_t leftValue, uint64_t rightValue) {
-   switch (op) {
-   case BinOp::Plus: return leftValue + rightValue;
-   case BinOp::Minus: return leftValue - rightValue;
-   case BinOp::Mult: return leftValue * rightValue;
-   case BinOp::And: return leftValue & rightValue;
-   case BinOp::Or: return leftValue | rightValue;
-   case BinOp::Xor: return leftValue ^ rightValue;
-   default: throw std::runtime_error("Unsupported binary operation");
-   }
-}
-
-// Evaluate formula on inputs
-uint64_t evaluateFormula(const Formula& formula, const std::vector<uint64_t>& inputs) {
-   return std::visit([&inputs](auto&& arg) -> uint64_t {
-      using T = std::decay_t<decltype(arg)>;
-
-      if constexpr (std::is_same_v<T, Variable>) {
-         return inputs[arg.var];
-      }
-      else if constexpr (std::is_same_v<T, UnaryOperation>) {
-         uint64_t operandValue = evaluateFormula(*arg.operand, inputs);
-         return evaluateUnaryOperation(arg.op, operandValue);
-      }
-      else if constexpr (std::is_same_v<T, BinaryOperation>) {
-         uint64_t leftValue = evaluateFormula(*arg.left, inputs);
-         uint64_t rightValue = evaluateFormula(*arg.right, inputs);
-         return evaluateBinaryOperation(arg.op, leftValue, rightValue);
-      }
-      }, formula.expr);
-}
-
 // Check whether a formula satisfies all examples
-bool testFormulaOnExamples(const Formula& formula, const std::vector<Example>& examples) {
+bool testFormulaOnExamples(const Formula& formula, const vector<Example>& examples) {
    for (const auto& example : examples) {
-      uint64_t result = evaluateFormula(formula, example.inputs);
+      uint64_t result = formula.evaluate(example.inputs);
       if (result != example.output) {
          return false;
       }
@@ -267,33 +129,33 @@ bool testFormulaOnExamples(const Formula& formula, const std::vector<Example>& e
 
 int main() {
 
-   auto formulas = generateFormulas(3);
-   // printFormulas(formulas);
+   const string filename = "example_8_and_16bits.json";
+   vector<Example> examples = readExamples(filename);
 
-   const std::string filename = "example_8_and_16bits.json";
-   std::vector<Example> examples = readExamples(filename);
+   auto formulas = generateFormulas(3);
+   printFormulas(formulas);
 
    // try {
-   //    std::vector<Example> examples = readExamples(filename);
+   //    vector<Example> examples = readExamples(filename);
 
    //    // Display the examples
    //    for (const auto& example : examples) {
-   //       std::cout << "Inputs: ";
+   //       cout << "Inputs: ";
    //       for (const auto& input : example.inputs) {
-   //          std::cout << std::hex << input << " "; // Print inputs in hex
+   //          cout << hex << input << " "; // Print inputs in hex
    //       }
-   //       std::cout << "Output: " << std::hex << example.output << std::endl; // Print output in hex
+   //       cout << "Output: " << hex << example.output << endl; // Print output in hex
    //    }
    // }
-   // catch (const std::exception& e) {
-   //    std::cerr << "Error: " << e.what() << std::endl;
+   // catch (const exception& e) {
+   //    cerr << "Error: " << e.what() << endl;
    // }
 
    for (const auto& formulaSet : formulas) {
       for (const auto& formula : formulaSet) {
          if (testFormulaOnExamples(*formula, examples)) {
-            std::cout << "Found a matching formula:\n";
-            printFormula(*formula);
+            cout << "Found a matching formula:\n";
+            formula->printFormula();
             break;
          }
       }
